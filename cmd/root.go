43388ae1cc05cc4d7bbe414b5ec550735f3a4b64
VincentBoillotDevalliere <vboillot@hotmail.fr>
2025-10-08 15:27:22 +0200

    chore: remove copyright comments from source files

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2025 VincentBoillotDevalliere
-*/
 package cmd
 
 import (
--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2025 VincentBoillotDevalliere
-*/
 package main
 
 import "github.com/VincentBoillotDevalliere/kafka-cli/cmd"
